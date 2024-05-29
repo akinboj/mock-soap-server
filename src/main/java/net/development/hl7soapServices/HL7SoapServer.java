8ee9bddca03ac06fac185f8ee40f19f00abb464e
Yemi <yemie@Yemis-MacBook-Pro.local>
2024-05-29 21:42:52 +1000

    Deployed to K8s

--- src/main/java/net/development/hl7soapServices/HL7SoapServer.java
+++ src/main/java/net/development/hl7soapServices/HL7SoapServer.java
@@ -1,21 +1,19 @@
 package net.development.hl7soapServices;
 
-import javax.xml.ws.Endpoint;
-
-import org.apache.cxf.Bus;
-import org.apache.cxf.BusFactory;
-import org.apache.cxf.jaxws.EndpointImpl;
-import org.apache.cxf.transport.http.HTTPTransportFactory;
-import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import jakarta.xml.ws.Endpoint;
+import jakarta.xml.ws.soap.SOAPBinding;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HL7SoapServer {
     private static final Logger logger = LoggerFactory.getLogger(HL7SoapServer.class);
+    private static final String HL7_SOAP_HOST = System.getenv("MY_POD_IP");
+//    private static final String HL7_SOAP_HOST = "192.168.0.17";
+    private static final int HL7_SOAP_PORT = 8080;
+    private static final int ENDPOINT_PUBLISHED_PORT = 8090;
 
     public static void main(String[] args) {
         try {
@@ -26,49 +24,29 @@ public class HL7SoapServer {
             // Create ServerConnector with the desired host and port
             logger.info("Creating ServerConnector...");
             ServerConnector connector = new ServerConnector(server);
-            connector.setHost("192.168.0.17");
-            connector.setPort(8080);
+            connector.setHost(HL7_SOAP_HOST);
+            connector.setPort(HL7_SOAP_PORT);
 
             // Add the ServerConnector to the server
             logger.info("Adding ServerConnector to the server...");
             server.addConnector(connector);
 
-            // Create CXF servlet without Spring
-            logger.info("Creating CXFNonSpringServlet...");
-            CXFNonSpringServlet cxfServlet = new CXFNonSpringServlet();
-
-            // Register and map the CXF servlet
-            logger.info("Registering and mapping the CXF servlet...");
-            ServletHolder servletHolder = new ServletHolder(cxfServlet);
+            // Create and map the servlet context
+            logger.info("Creating servlet context...");
             ServletContextHandler context = new ServletContextHandler();
             context.setContextPath("/");
-            context.addServlet(servletHolder, "/*");
             server.setHandler(context);
 
             // Start the Jetty server
             logger.info("Starting the Jetty server...");
             server.start();
 
-            // Initialize the CXF bus
-            logger.info("Initializing CXF bus...");
-            Bus bus = BusFactory.getDefaultBus();
-            logger.info("CXF bus initialized.");
-
-            // Configure the HTTP transport
-            logger.info("Configuring HTTP transport...");
-            HTTPTransportFactory transportFactory = new HTTPTransportFactory();
-            bus.setExtension(transportFactory, HTTPTransportFactory.class);
-            logger.info("HTTP transport configured.");
-
-            // Create and publish the SOAP endpoint
+            // Create and publish the SOAP endpoint with SOAP 1.2 binding
             logger.info("Creating and publishing SOAP endpoint...");
             HL7SoapServiceImplementation serviceImpl = new HL7SoapServiceImplementation();
-            EndpointImpl endpoint = (EndpointImpl) Endpoint.create(serviceImpl);
-            endpoint.setBus(bus);
-            endpoint.publish("/hl7-soap-service");
-            logger.info("SOAP endpoint published.");
-
-            logger.info("HL7 SOAP service started at http://192.168.0.17:8080/hl7-soap-service");
+            Endpoint endpoint = Endpoint.create(SOAPBinding.SOAP12HTTP_BINDING, serviceImpl);
+            endpoint.publish("http://"+HL7_SOAP_HOST+":"+ENDPOINT_PUBLISHED_PORT+"/services/hl7-soap");
+            logger.info("SOAP endpoint published at http://"+HL7_SOAP_HOST+":"+ENDPOINT_PUBLISHED_PORT+"/services/hl7-soap");
 
             // Join the server thread to keep the server running
             logger.info("Joining the server thread...");
